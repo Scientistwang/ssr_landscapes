9eafad0046876f3b45cd40c5fb3def53e66c8120
Eric <jones.eric93@gmail.com>
2018-08-23 00:13:53 -0700

    added files

--- bistable_ssr.py
+++ bistable_ssr.py
@@ -53,6 +53,17 @@ class originalParams:
         jac[1][1] = -s.gamma
         return jac
 
+    def convert_sp_to_op(s, points):
+        """ Take a list of 2D points (corresponding to ssrParams) and
+        output a list of the corresponding 2D points in the original basis. 
+        """
+        new_points = []
+        for point in points:
+            new_point = s.ssa*point[0] + s.ssb*point[1]
+            new_points.append(new_point)
+        new_points = np.array(new_points)
+        return new_points
+
 
     def get_nullclines(s):
         """ Returns nullclines of bistable switch dynamical system """
@@ -98,8 +109,8 @@ class originalParams:
             color = 'k' if is_stable else 'r'
             ax.plot(fp[0], fp[1], c=color, marker='.', ms=25, zorder=4)
 
-        ax.plot(x_nullcline[:,0], x_nullcline[:,1], label='$\dot{x} = 0$')
-        ax.plot(y_nullcline[:,0], y_nullcline[:,1], label='$\dot{y} = 0$')
+        ax.plot(x_nullcline[:,0], x_nullcline[:,1], c='grey', label='$\dot{x} = 0$')
+        ax.plot(y_nullcline[:,0], y_nullcline[:,1], c='grey', label='$\dot{y} = 0$')
         ax.legend(); ax.set_xlabel('gene $x$ concentration'); ax.set_ylabel('gene $y$ concentration')
         ax.set_title('Nullclines with $\\alpha =${:.3}, $\\beta = ${:.3}, and $\gamma = ${:.3}'
                      .format(s.alpha, s.beta, s.gamma), fontsize=18)
@@ -176,221 +187,21 @@ class originalParams:
 
         return stab_fps
 
-class quasipolynomialParams:
-    """ This class contains all methods relevant for the quasipolynomial
-    differential equation form of the bistable switch, which consists of 8
-    coupled ODEs, each of which may be up to fourth order. """
-    def __init__(s, op=originalParams()):
-        s.alpha, s.beta, s.gamma = op.alpha, op.beta, op.gamma
-        s.ext = 'a{:.3}_b{:.3}_g{:.3}'.format(s.alpha, s.beta, s.gamma)
-
-        s.ssa = s.convert_op_to_qp([op.ssa])[0]
-        s.ssb = s.convert_op_to_qp([op.ssb])[0]
-        s.fps = np.array([s.ssa, s.ssb])
-        #print(s.ssa[4], s.ssa[5], s.ssa[6], s.ssa[7])
-        #print(s.ssa[4]*s.ssa[6])
-        #print(s.ssa[5]*s.ssa[7])
-
-    def get_integrand(s, Y, t):
-        """ Synthetic gene circuit for a bistable switch in quasipolynomial
-        form. To "simplify" this equation back to the original 2-dimensional
-        ODE form, set u=1/y, v=1/z, a=1/(1+v^2), and b=1/(1+u^2).  This
-        integrand returns the derivative of the vector [u, v, a, b] """
-        return np.array([Y[0]*(1 - s.alpha*Y[0] + s.alpha*Y[0]*Y[2]),
-                         Y[1]*(s.gamma - s.beta*Y[1] + s.beta*Y[1]*Y[3]),
-                         Y[2]*(-2*s.gamma*Y[1]**2*Y[2] + 2*s.beta*Y[1]**3*Y[2]
-                               - 2*s.beta*Y[1]**3*Y[2]*Y[3]),
-                         Y[3]*(-2*Y[0]**2*Y[3] + 2*s.alpha*Y[0]**3*Y[3]
-                               - 2*s.alpha*Y[0]**3*Y[2]*Y[3])])
-
-    def get_jacobian(s, fp):
-        """ Return the jacobian of the bistable switch in quasipolynomial
-        coordinates evaluated at the fixed point fp. Variables are [y, z, a, b,
-        c, d, e, f]. """
-        u,v,a,b = fp
-        jac = np.array(
-            [[1 - 2*s.alpha*u + 2*s.alpha*u*a, 0, s.alpha*u**2, 0],
-             [0, s.gamma - 2*s.beta*v + 2*s.beta*v*b, 0, s.beta*v**2],
-             [0, -4*s.gamma*a**2*v + 6*s.beta*a**2*v**2 - 6*s.beta*a**2*v**2*b,
-              -4*s.gamma*a*v**2 + 4*s.beta*a*v**3 - 4*s.beta*a*b*v**3,
-              -2*s.beta*a**2*v**3],
-             [-4*b**2*u + 6*s.alpha*b**2*u**2 - 6*s.alpha*a*b**2*u**2, 0,
-              -2*s.alpha*b**2*u**3, -4*b*u**2 + 4*s.alpha*b*u**3 -
-              4*s.alpha*a*b*u**3]])
-        return jac
-
-    def convert_op_to_qp(s, points):
-        """ Take a list of 2D points (corresponding to originalParams) and
-        output a list of the corresponding 8D points in the quasipolynomial
-        basis [u, v, a, b]. Recall that u=1/y, v=1/z, a=1/(1+v^2), and
-        b=1/(1+u^2) """
-        new_points = []
-        for point in points:
-            y = point[0]
-            z = point[1]
-            u = 1/y
-            v = 1/z
-            new = [u, v, 1/(1+v**2), 1/(1+u**2)]
-            new_points.append(new)
-        new_points = np.array(new_points)
-        return new_points
-
-    def plot_example_traj(s, ax, savefig=True):
-        """ Plot an identical sample trajectory as the one in the
-        originalParams class"""
-        op_ic = np.array([1.8, 3])
-        qp_ic = s.convert_op_to_qp([op_ic])[0]
-
-        t = np.linspace(0, 500, 501)
-        z = integrate.odeint(s.get_integrand, qp_ic, t)
-        if False:
-            print('qp trajectory:')
-            print(z[:, :2])
-        x = np.array([1/zz[0] for zz in z])
-        y = np.array([1/zz[1] for zz in z])
-        ax.plot(x, y, ':b', zorder=3, label='QP form')
-        ax.legend(fontsize=12)
-
-        filename = 'figs/qp_example_traj_{}.pdf'.format(s.ext)
-        if savefig:
-            plt.savefig(filename, bbox_inches='tight')
-            print('... saved to {}'.format(filename))
-
-class gLVParams:
-    """ This class contains all methods relevant for the gLV 
-    differential equation form of the bistable switch, which consists of 6
-    coupled ODEs, each of which may be up to second order. """
-    def __init__(s, qp=quasipolynomialParams()):
-        s.alpha, s.beta, s.gamma = qp.alpha, qp.beta, qp.gamma
-
-        s.mu = np.array([1, s.gamma, 1, s.gamma, 2*s.gamma, 3*s.gamma,
-                         3*s.gamma, 2, 3, 3])
-        s.M = np.array(
-               [[-s.alpha, 0, s.alpha, 0, 0, 0, 0, 0, 0, 0],
-                [0, -s.beta, 0, s.beta, 0, 0, 0, 0, 0, 0],
-                [-s.alpha, 0, s.alpha, 0, -2*s.gamma, 2*s.beta, -2*s.beta, 0,
-                 0, 0],
-                [0, -s.beta, 0, s.beta, 0, 0, 0, -2, 2*s.alpha, -2*s.alpha],
-                [0, -2*s.beta, 0, 2*s.beta, -2*s.gamma, 2*s.beta, -2*s.beta, 0,
-                 0, 0],
-                [0, -3*s.beta, 0, 3*s.beta, -2*s.gamma, 2*s.beta, -2*s.beta, 0,
-                 0, 0],
-                [0, -3*s.beta, 0, 3*s.beta, -2*s.gamma, 2*s.beta, -2*s.beta, -2,
-                 2*s.alpha, -2*s.alpha],
-                [-2*s.alpha, 0, 2*s.alpha, 0, 0, 0, 0, -2, 2*s.alpha,
-                 -2*s.alpha],
-                [-3*s.alpha, 0, 3*s.alpha, 0, 0, 0, 0, -2, 2*s.alpha,
-                 -2*s.alpha],
-                [-3*s.alpha, 0, 3*s.alpha, 0, -2*s.gamma, 2*s.beta, -2*s.beta,
-                 -2, 2*s.alpha, -2*s.alpha]])
-
-        s.ssa = s.convert_qp_to_gp([qp.ssa])[0]
-        s.ssb = s.convert_qp_to_gp([qp.ssb])[0]
-        s.fps = np.array([s.ssa, s.ssb])
-        #print(s.ssa)
-        #print(s.ssb)
-
-        s.ext = 'a{:.3}_b{:.3}_g{:.3}'.format(s.alpha, s.beta, s.gamma)
-
-    def get_integrand(s, Y, t):
-        """ Synthetic gene circuit for a bistable switch in gLV
-        form. To "simplify" this equation back to the quasipolynomial
-        form, set c=u*a, d=v*b, e=v^2*a, f=v^3*a, g=v^3*a*b, h=u^2*b, k=u^3*b,
-        l=u^3*a*b.  This integrand returns the derivative of the vector
-        [u,v,c,d,e,f,g,h,k,l]. """
-        return np.dot(np.diag(s.mu), Y) + np.dot( np.diag(np.dot(s.M, Y)), Y)
-
-    def get_jacobian(s, fp):
-        """ Return the jacobian of the bistable switch in quasipolynomial
-        coordinates evaluated at the fixed point fp. """
-        N = len(fp)
-        x = fp
-        jac = np.zeros((N, N))
-        for i in range(N):
-            for j in range(N):
-                if i is j:
-                    val = s.mu[i] + np.dot(s.M, x)[i] + s.M[i,i]*x[i]
-                    jac[i, j] = val
-                else:
-                    val = x[i]*s.M[i,j]
-                    jac[i, j] = val
-        return jac
-
-    def convert_qp_to_gp(s, points):
-        """ Take a list of 8D points (corresponding to quasipolynomialParams)
-        and output a list of the corresponding 6D points in the gLV basis
-        ([u,v,c,d,e,f,g,h,k,l]). Recall that c=u*a, d=v*b, e=v^2*a, f=v^3*a,
-        g=v^3*a*b, h=u^2*b, k=u^3*b, l=u^3*a*b"""
-        new_points = []
-        for point in points:
-            u,v,a,b = point
-            new = [u, v, u*a, v*b, v**2*a, v**3*a, v**3*a*b, u**2*b, u**3*b,
-                   u**3*a*b]
-            new_points.append(new)
-        new_points = np.array(new_points)
-        return new_points
-
-    def plot_example_traj(s, ax, savefig=True):
-        """ Plot an identical sample trajectory as the one in the
-        originalParams class"""
-        op_ic = np.array([1.8, 3])
-        qp = quasipolynomialParams()
-        qp_ic = qp.convert_op_to_qp([op_ic])[0]
-        gp_ic = s.convert_qp_to_gp([qp_ic])[0]
-
-        t = np.linspace(0, 500, 501)
-        z = integrate.odeint(s.get_integrand, gp_ic, t)
-        if False:
-            print('gp trajectory:')
-            print(z[:, :2])
-        x = np.array([1/zz[0] for zz in z])
-        y = np.array([1/zz[1] for zz in z])
-        ax.plot(x, y, '-.r', zorder=2, label='gLV form')
-        ax.legend()
-
-        filename = 'figs/gp_example_traj_{}.pdf'.format(s.ext)
-        if savefig:
-            plt.savefig(filename, bbox_inches='tight')
-            print('... saved to {}'.format(filename))
-
-
 class ssrParams:
     """ This class contains all methods relevant for the steady state reduction
     of the gLV differential equation form of the bistable switch. """
-    def __init__(s, gp=gLVParams()):
+    def __init__(s, op=originalParams()):
 
-        s.alpha, s.beta, s.gamma = gp.alpha, gp.beta, gp.gamma
-        s.gp_ssa = gp.ssa
-        s.gp_ssb = gp.ssb
+        s.alpha, s.beta, s.gamma = op.alpha, op.beta, op.gamma
+        s.op_ssa = op.ssa
+        s.op_ssb = op.ssb
 
-        print('gLV steady states:')
-        print(gp.ssa)
-        print(gp.ssb)
-        print()
-
-        s.ssa = s.convert_gp_to_sp([s.gp_ssa])[0]
-        s.ssb = s.convert_gp_to_sp([s.gp_ssb])[0]
+        s.ssa = s.convert_op_to_sp([s.op_ssa])[0]
+        s.ssb = s.convert_op_to_sp([s.op_ssb])[0]
         s.fps = np.array([s.ssa, s.ssb])
 
-        s.mu = np.array([np.dot(gp.mu, gp.ssa), np.dot(gp.mu, gp.ssb)])
-        s.M = np.array([[np.dot(gp.ssa, np.dot(gp.M, gp.ssa)), np.dot(gp.ssa, np.dot(gp.M, gp.ssb))],
-                        [np.dot(gp.ssb, np.dot(gp.M, gp.ssa)), np.dot(gp.ssb, np.dot(gp.M, gp.ssb))]])
-        print('extra:')
-        print(gp.ssa)
-        print(gp.ssb)
-        print(np.dot(gp.M, gp.ssa))
-        print(np.dot(gp.M, gp.ssb))
-        print()
-        print('gLV mu and M:')
-        print(gp.mu)
-        a = gp.M.copy()
-        a[[1, 2], :] = a[[2, 1], :]
-        a[:, [1, 2]] = a[:, [2, 1]]
-        print(gp.M)
-        print()
-        print('SSR mu and M:')
-        print(s.mu)
-        print(s.M)
+        s.mu, s.M = s.get_ssr_params(op)
+
         s.ext = 'a{:.3}_b{:.3}_g{:.3}'.format(s.alpha, s.beta, s.gamma)
 
     def get_integrand(s, Y, t):
@@ -417,19 +228,34 @@ class ssrParams:
                     jac[i, j] = val
         return jac
 
-    def convert_gp_to_sp(s, points):
-        """ Take a list of 6D points (corresponding to gLVParams) and
-        output a list of the corresponding 2D points in the SSR
-        basis ([x, y]). """
+    def convert_op_to_sp(s, points):
+        """ Take a list of 2D points (corresponding to originalParams) and
+        output a list of the corresponding 2D points in the SSR basis ([x, y]).
+        """
         new_points = []
         for point in points:
-            uu = np.dot(s.gp_ssa, s.gp_ssa); vv = np.dot(s.gp_ssb, s.gp_ssb)
-            xu = np.dot(point, s.gp_ssa); xv = np.dot(point, s.gp_ssb)
-            uv = np.dot(s.gp_ssa, s.gp_ssb)
+            uu = np.dot(s.op_ssa, s.op_ssa); vv = np.dot(s.op_ssb, s.op_ssb)
+            xu = np.dot(point, s.op_ssa); xv = np.dot(point, s.op_ssb)
+            uv = np.dot(s.op_ssa, s.op_ssb)
             new_points.append([(xu*vv - xv*uv)/(uu*vv - uv**2),
                                (uu*xv - xu*uv)/(uu*vv - uv**2)])
         new_points = np.array(new_points)
         return new_points
+
+    def get_ssr_params(s, op):
+        ya,za = op.ssa
+        yb,zb = op.ssb
+        M_aa = (-ya - 2*op.alpha*za**2/(1 + za**2)**2
+                - 2*op.beta*ya**2/(1+ya**2)**2 - op.gamma*za)
+        M_ab = (-yb - 2*op.alpha*za*zb/(1 + za**2)**2
+                - 2*op.beta*ya*yb/(1+ya**2)**2 - op.gamma*zb)
+        M_ba = (-ya - 2*op.alpha*za*zb/(1 + zb**2)**2
+                - 2*op.beta*ya*yb/(1+yb**2)**2 - op.gamma*za)
+        M_bb = (-yb - 2*op.alpha*zb**2/(1 + zb**2)**2
+                - 2*op.beta*yb**2/(1+yb**2)**2 - op.gamma*zb)
+        M = np.array([[M_aa, M_ab], [M_ba, M_bb]])
+        mu = np.array([-M_aa, -M_bb])
+        return mu, M
 
     def plot_example_traj(s, ax, savefig=True):
         """ Plot an identical sample trajectory as the one in the
@@ -463,80 +289,101 @@ class ssrParams:
             plt.savefig(filename, bbox_inches='tight')
             print('... saved to {}'.format(filename))
 
+def compare_trajectories(op, sp):
+    """ Plot a sample trajectory in both originalParams and in ssrParams"""
+    fig, ax = plt.subplots(figsize=(6,6))
+    op.plot_nullclines(ax, savefig=False)
+
+    for ic in [[.5, .5], [1.4, 3], [1, 4], [4, 1], [2, 3], [.35, 1.2]]:
+        op_ic = np.array(ic)
+        sp_ic = sp.convert_op_to_sp([op_ic])[0]
+        print('originalParams IC is {}'.format(op_ic))
+        print('ssrParams IC is {}'.format(sp_ic))
+
+        t = np.linspace(0, 100, 501)
+        orig_z = integrate.odeint(op.get_integrand, op_ic, t)
+        ssr_z = integrate.odeint(sp.get_integrand, sp_ic, t)
+
+        orig_z_ssr_basis = sp.convert_op_to_sp(orig_z)
+        ssr_z_orig_basis = op.convert_sp_to_op(ssr_z)
+
+        #print(orig_z)
+        #print(ssr_z_orig_basis)
+        #print(ssr_z)
+        #print(orig_z_ssr_basis)
+
+        ax.plot(op_ic[0], op_ic[1], 'k.', ms=17)
+        ax1, = ax.plot(orig_z[:,0], orig_z[:,1], color='green', ls='--', zorder=2, label='original form')
+        ax2, = ax.plot(ssr_z_orig_basis[:,0], ssr_z_orig_basis[:,1],
+                       color='purple', ls='-.', zorder=2, label='SSR form')
+        ax.legend([ax1, ax2], ['original form', 'SSR form'])
+        ax.axis([0, None, 0, None])
+
+    filename = 'figs/orig_basis_traj_{}.pdf'.format(op.ext)
+    plt.savefig(filename, bbox_inches='tight')
+    print('... saved to {}'.format(filename))
+
+
 
 def verify_all_fps_are_fps():
     """ Test that fixed points of the originalParams translate to also being
     fixed points of the quasipolynomialParams and gLVParams """
     print('TESTING fixed points are preserved in all algebraic forms')
     op = originalParams()
-    qp = quasipolynomialParams(op)
-    gp = gLVParams(qp)
-    sp = ssrParams(gp)
+    sp = ssrParams(op)
     fps = op.get_stable_fps()
-    qp_fps = qp.convert_op_to_qp(fps)
-    gp_fps = gp.convert_qp_to_gp(qp_fps)
-    sp_fps = sp.convert_gp_to_sp(gp_fps)
+    sp_fps = sp.convert_op_to_sp(fps)
     verbose = True
-    for op_fp,qp_fp,gp_fp,sp_fp in zip(fps, qp_fps, gp_fps, sp_fps):
+    for op_fp,sp_fp in zip(fps, sp_fps):
         if verbose:
-            print(op.get_integrand(op_fp, 0))
-            print(qp.get_integrand(qp_fp, 0))
-            print(gp.get_integrand(gp_fp, 0))
-            print(sp.get_integrand(sp_fp, 0))
-            print()
+            print('    originalParams f.p. {} integrand: {}'.format(op_fp,op.get_integrand(op_fp, 0)))
+            print('    ssrParams f.p. {} integrand: {}'.format(sp_fp,sp.get_integrand(sp_fp, 0)))
 
 def verify_all_fps_are_stable():
     """ Test that the fixed points are stable in each system """
-    print('TESTING fixed points are stable in all algebraic forms')
+    print('TESTING fixed points are stable in all forms')
     op = originalParams()
-    qp = quasipolynomialParams(op)
-    gp = gLVParams(qp)
-    sp = ssrParams(gp)
+    sp = ssrParams(op)
 
-    for p in [op, qp, gp, sp]:
-        print(p.fps)
+    for p,p_name in zip([op, sp], ['originalParams', 'ssrParams']):
+        print('    {} fps are {} and {} with eigenvalues:'.format(p_name, p.fps[0], p.fps[1]))
         for fp in p.fps:
-            print(np.linalg.eig(p.get_jacobian(fp))[0])
-        print()
+            print('      {}'.format(np.linalg.eig(p.get_jacobian(fp))[0]))
 
 def verify_all_trajs_are_same():
     """ Plot the same trajectory in the three different forms (op, qp, gp) """
     print('TESTING all algebraic forms lead to same trajectory')
     op = originalParams()
-    qp = quasipolynomialParams(op)
-    print('ss are at u, v=')
-    print(qp.ssa[0:2])
-    print(qp.ssb[0:2])
-    print(qp.ssa[0]*qp.ssb[1] - qp.ssa[1]*qp.ssb[0]*qp.gamma)
-
-    gp = gLVParams(qp)
-    sp = ssrParams(gp)
+    sp = ssrParams(op)
     fig, ax = plt.subplots(figsize=(6,6))
     op.plot_nullclines(ax, savefig=False)
-    op.plot_example_traj(ax, savefig=False)
-    qp.plot_example_traj(ax, savefig=False)
-    gp.plot_example_traj(ax, savefig=False)
     sp.plot_example_traj(ax, savefig=True)
 
 
 def sanity_checks():
     """ Run sanity check tests that ensure that algebraic manipulations don't
     change trajectories/fixed points/stability """
-    #verify_all_fps_are_fps()
+    verify_all_fps_are_fps()
+    print('----')
+    verify_all_fps_are_stable()
     #print('----')
-    #verify_all_fps_are_stable()
-    #print('----')
-    verify_all_trajs_are_same()
+    #verify_all_trajs_are_same()
 
 if __name__ == '__main__':
-    sanity_checks()
+    #sanity_checks()
 
-    #op = originalParams()
-    #qp = quasipolynomialParams(op)
-    #gp = gLVParams(qp)
-    #sp = ssrParams(gp)
+    op = originalParams()
+    sp = ssrParams(op)
 
+    compare_trajectories(op, sp)
 
+    print('----')
+
+    print('originalParams steady states: {}, {}'.format(op.ssa, op.ssb))
+    print('ssrParams steady states: {}, {}'.format(sp.ssa, sp.ssb))
+    print('ssrParams mu and M:')
+    print(sp.mu)
+    print(sp.M)
     #fig, ax = plt.subplots(figsize=(6,6))
     #x_null, y_null = op.get_nullclines()
     #true_fps = op.get_intersections(x_null, y_null)
